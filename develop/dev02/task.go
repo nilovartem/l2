3d1439adec941404aaedec8e76787639a766d3d6
Artem <nilovtema@yandex.ru>
2023-04-26 13:05:01 +0300

    Add dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,23 +1,50 @@
 package main
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
 Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
-	- "a4bc2d5e" => "aaaabccddddde"
-	- "abcd" => "abcd"
-	- "45" => "" (некорректная строка)
-	- "" => ""
+  - "a4bc2d5e" => "aaaabccddddde"
+  - "abcd" => "abcd"
+  - "45" => "" (некорректная строка)
+  - "" => ""
+
 Дополнительное задание: поддержка escape - последовательностей
-	- qwe\4\5 => qwe45 (*)
-	- qwe\45 => qwe44444 (*)
-	- qwe\\5 => qwe\\\\\ (*)
+  - qwe\4\5 => qwe45 (*)
+  - qwe\45 => qwe44444 (*)
+  - qwe\\5 => qwe\\\\\ (*)
 
 В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
 
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
-
-func main() {
-
+func Unpack(s string) (r string, err error) {
+	const slash = "\\"
+	if _, err := strconv.Atoi(s); err == nil {
+		return r, errors.New("некорректная строка")
+	}
+	var last rune
+	var escaped bool
+	var b strings.Builder //самый эффективный способ работы со строками
+	for _, char := range s {
+		if unicode.IsDigit(char) && !escaped {
+			m := int(char - '0')
+			r := strings.Repeat(string(last), m-1)
+			b.WriteString(r)
+		} else {
+			escaped = string(char) == slash && string(last) != slash
+			if !escaped {
+				b.WriteRune(char)
+			}
+			last = char
+		}
+	}
+	return b.String(), err
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,35 @@
 package main
+
+import "testing"
+
+func TestIncorrent(t *testing.T) {
+	const testCase = ""
+	res, err := Unpack(testCase)
+	if err != nil {
+		t.Errorf("Expected error, got %v", res)
+	}
+}
+func TestDigit(t *testing.T) {
+	const testCase = "a4bc2d5e"
+	const expected = "aaaabccddddde"
+	res, err := Unpack(testCase)
+	if res != expected {
+		t.Errorf("Expected %v, got %v, error is %v", expected, res, err)
+	}
+}
+func TestBase(t *testing.T) {
+	const testCase = "abcd"
+	const expected = "abcd"
+	res, err := Unpack(testCase)
+	if res != expected {
+		t.Errorf("Expected %v, got %v, error is %v", expected, res, err)
+	}
+}
+func TestEscaped(t *testing.T) {
+	const testCase = "qwe\\45"
+	const expected = "qwe44444"
+	res, err := Unpack(testCase)
+	if res != expected {
+		t.Errorf("Expected %v, got %v, error is %v", expected, res, err)
+	}
+}
